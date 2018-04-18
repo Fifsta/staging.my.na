0fa3e58af1f08a8a81e791a06355ec9d7c075c46
benwork <benpiet@intouch.com.na>
2018-04-18 11:48:12 +0200

    no message

--- application/views/inc/adverts.php
+++ application/views/inc/adverts.php
@@ -1,6 +1,3 @@
-			<div class="adverts">
-				<div><a href="#"><img src="images/advert-sample.png" class="img-fluid"></a></div>
-				<div><a href="#"><img src="images/advert-sample.png"></a></div>
-				<div><a href="#"><img src="images/advert-sample.png"></a></div>
-				<div><a href="#"><img src="images/advert-sample.png"></a></div>
-			</div>
+<div class="adverts">
+
+</div>
