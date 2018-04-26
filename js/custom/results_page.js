71f65c2b851df28c7249302070fb87f1aac22c98
benwork <benpiet@intouch.com.na>
2018-04-26 10:36:59 +0200

    no message

--- js/custom/results_page.js
+++ js/custom/results_page.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 			      $.ajax({
 			        'async': false,
 			        'type': "get",
-			        'url': base+"map/results/"+cat_id,
+			        'url': base+"map/results/"+cat_id+"/sub",
 			        'dataType': "json",
 			        'success': function (data) {
 			          json = data;
