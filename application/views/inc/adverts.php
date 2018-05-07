b3aebb13af404fa87cf47e539709b5b145b934b5
benwork <benpiet@intouch.com.na>
2018-05-07 09:41:47 +0200

    no message

--- application/views/inc/adverts.php
+++ application/views/inc/adverts.php
@@ -1,4 +1,4 @@
-<div class="adverts" id="advert-box">
+<div class="adverts hidden-sm-down" id="advert-box">
 	
 		<!--<img alt="Feature Your Listing Online" src="https://www.my.na/img/adverts/featured_listing_banner.png" class="img-fluid">-->
 
