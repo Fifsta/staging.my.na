4971e8b14408a29814617bc848613277e8e42733
benwork <benpiet@intouch.com.na>
2018-05-16 10:25:14 +0200

    no message

--- application/views/trade/inc/product_map_inc.php
+++ application/views/trade/inc/product_map_inc.php
@@ -10,20 +10,13 @@ $map_details = $this->trade_model->get_map_details($ID);
 
 if(count($map_details) > 0){
   
-  if($map_details['PRODUCT_MAP_LATITUDE'] == ''){
-    
-    $lat = '-22.583741';
-    $long = '17.093782';
-    $zoom = '7'; 
-    echo '<script type="text/javascript">$("#map_info").slideDown();</script>';
-    
-  }else{
+
     
     $lat = $map_details['PRODUCT_MAP_LATITUDE'];
     $long = $map_details['PRODUCT_MAP_LONGITUDE'];
     $zoom = '12';
     
-  } 
+  
 
 ?>
 
