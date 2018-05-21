d8d77f71e1e4deac7de0a35194f0b3e104453b52
IHMS-ASPIRE <rolandihms@gmail.com>
2018-05-21 10:16:47 +0200

    added rename uppercase scripts in controller/models dir

--- application/controllers/rename.sh
+++ application/controllers/rename.sh
@@ -3,5 +3,4 @@ for f in **.php; do
   fn=$(basename "$f") # remove directory name
   Fn=${fn^}           # uppercase first letter
   mv -- "$f" "$(dirname "$f")/$Fn"  # combine both
-  git mv "$f" "$Fn"
 done
