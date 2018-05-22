e44fd6a3eaf514640c517c9f731afa08df3e3267
benwork <benpiet@intouch.com.na>
2018-05-22 11:38:45 +0200

    no message

--- application/views/trade/list/step4.php
+++ application/views/trade/list/step4.php
@@ -4,17 +4,17 @@
 	//+++++++++++++++++++++++++++++++++
 
 	$str = '';
-if (isset($bus_id) && $bus_id != 0) {
+	if (isset($bus_id) && $bus_id != 0) {
 
-    $str = ' for Business';
+	    $str = ' for Business';
 
-    $all_link = site_url('/') . 'members/business/' . $bus_id . '/';
+	    $all_link = site_url('/') . 'members/business/' . $bus_id . '/';
 
-} else {
+	} else {
 
-    $all_link = site_url('/') . 'members/my_products/';
+	    $all_link = site_url('/') . 'members/my_products/';
 
-}
+	}
 
 ?>
 <div id="anchor_me"></div>
@@ -50,38 +50,26 @@ if (isset($bus_id) && $bus_id != 0) {
 
 	    ?>
 
-        <div class="alert">
-            <h4>No extras available for the selected category</h4>
-            There are no extras available to set for the selected category. Please make sure the product description describes the product to its fullest
-        </div>
-        <div>
-           <a href="javascript:void(0)" onclick="proceed_to_5()" id="proceed_to_5" class="btn btn-success btn-lg pull-right">Next <i class="fa fa-chevron-right text-light"></i></a>
-           <a href="<?php echo $all_link; ?>#Latest" id="back_to_all" class="btn btn-lg btn-dark pull-right" style="margin-right:5px">All Products</a>
-           <a href="javascript:void(0)"onclick="back_to_3()" id="back_to_3" class="btn btn-lg btn-warning pull-right" style="margin-right:5px"><i class="fa fa-chevron-left text-dark"></i> Back</a>
-       	   <div class="clearfix">&nbsp;</div>	
-        </div>
+	        <div class="alert">
+	            <h4>No extras available for the selected category</h4>
+	            There are no extras available to set for the selected category. Please make sure the product description describes the product to its fullest
+	        </div>
+	        <div>
+	           <a href="javascript:void(0)" onclick="proceed_to_5()" id="proceed_to_5" class="btn btn-success btn-lg pull-right">Next <i class="fa fa-chevron-right text-light"></i></a>
+	           <a href="<?php echo $all_link; ?>#Latest" id="back_to_all" class="btn btn-lg btn-dark pull-right" style="margin-right:5px">All Products</a>
+	           <a href="javascript:void(0)"onclick="back_to_3()" id="back_to_3" class="btn btn-lg btn-warning pull-right" style="margin-right:5px"><i class="fa fa-chevron-left text-dark"></i> Back</a>
+	       	   <div class="clearfix">&nbsp;</div>	
+	        </div>
         
-    <?php } ?>
+    	<?php } ?>
 
     </div>
 </div>
 <div class="spacer"></div>
 
-
-
-    
-    <script data-cfasync="false" type="text/javascript">
+<script data-cfasync="false" type="text/javascript">
 	
 	$(document).ready(function(e) {
-
-		//window.scrollTo(0,$('#anchor_me').offset().top);
-		<?php
-		 /**
-		++++++++++++++++++++++++++++++++++++++++++++
-		//SUBMIT STEP 3
-		++++++++++++++++++++++++++++++++++++++++++++	
-		 */
-		 ?> 
 		
 		$('#add_extras_btn').bind('click', function(e){ 
 			
@@ -90,60 +78,41 @@ if (isset($bus_id) && $bus_id != 0) {
 			btn.html('Working...');
 			//cont.addClass('loading_img'); 
 			$.ajax({
-					type: 'post',
-					cache: false,
-					data: frm.serialize(),
-					url: '<?php echo site_url('/').'trade/add_extras';?>' ,
-					success: function (data) {
-						    btn.html('Update Item <i class="icon-chevron-right icon-white"></i>');
-							cont.removeClass('slideLeft'); 
-							cont.html(data);
-							proceed_to_5();
-							
-					}
-				});
-		});		
-		
+				type: 'post',
+				cache: false,
+				data: frm.serialize(),
+				url: '<?php echo site_url('/').'trade/add_extras';?>' ,
+				success: function (data) {
+					    btn.html('Update Item <i class="icon-chevron-right icon-white"></i>');
+						cont.removeClass('slideLeft'); 
+						cont.html(data);
+						proceed_to_5();
+						
+				}
+			});
+		});				
     });
 
-<?php
- /**
-++++++++++++++++++++++++++++++++++++++++++++
-//EXTRAS
-++++++++++++++++++++++++++++++++++++++++++++
-++++++++++++++++++++++++++++++++++++++++++++	
- */
- ?> 	  
-	  
+    
 	function proceed_to_5(){
-		
-			var cont = $('#admin_content').addClass('slideLeft');
-			  $.get('<?php echo site_url('/'). 'sell/step5/'.$product_id.'/'. $bus_id;?>', function(data) {
-					cont.removeClass('slideLeft').html(data);
-					
-			  });
-		
+
+		var cont = $('#admin_content').addClass('slideLeft');
+		$.get('<?php echo site_url('/'). 'sell/step5/'.$product_id.'/'. $bus_id;?>', function(data) {
+			cont.removeClass('slideLeft').html(data);
+		});
+
 	}
-	
-	<?php
-	   /**
-	  ++++++++++++++++++++++++++++++++++++++++++++
-	  //BACK
-	  ++++++++++++++++++++++++++++++++++++++++++++	
-	   */
-	   ?> 
-	  
-	  function back_to_3(){
-		  
-			  var cont = $('#admin_content').addClass('slideLeft'), btn = $('#back_to_3');
-			  btn.html('Working...');
-			  $.get('<?php echo site_url('/'). 'sell/step3/'.$product_id .'/'. $bus_id ;?>', function(data) {
-					cont.removeClass('slideLeft').html(data);
-					btn.html('<i class="icon-chevron-left icon-white"></i> Back');
-					
-			  });
-		  
-	  }
 
 
-	</script>
+	function back_to_3(){
+
+		var cont = $('#admin_content').addClass('slideLeft'), btn = $('#back_to_3');
+		btn.html('Working...');
+		$.get('<?php echo site_url('/'). 'sell/step3/'.$product_id .'/'. $bus_id ;?>', function(data) {
+			cont.removeClass('slideLeft').html(data);
+			btn.html('<i class="icon-chevron-left icon-white"></i> Back');
+		});
+
+	}
+
+</script>
