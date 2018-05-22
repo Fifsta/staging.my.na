6d1472020e75b592cd4f7a33d785f79c808bfec8
benwork <benpiet@intouch.com.na>
2018-05-22 15:24:46 +0200

    no message

--- application/views/members/verify.php
+++ application/views/members/verify.php
@@ -1,88 +1,119 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>
-<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>
-<html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>
-<html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js" lang="en"> <!--<![endif]-->
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="description" content="">
-	<meta name="author" content="">
-	<title>My Namibia - Verify Your Account</title>
-	<meta name="viewport" content="width=device-width">
+<!doctype html>
+<html lang="en">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
 
-	<link rel="stylesheet" href="<?php echo base_url('/'); ?>css/bootstrap.min.css">
-	<link rel="stylesheet" href="<?php echo base_url('/'); ?>css/skin1-front.css">
-	<link rel="shortcut icon" href="<?php echo base_url('/'); ?>favicon.ico">
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url('/'); ?>img/icons/my_na_[144x144].png">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url('/'); ?>img/icons/my_na_[114x114].png">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url('/'); ?>img/icons/my_na_[72x72].png">
-	<link rel="apple-touch-icon-precomposed" href="<?php echo base_url('/'); ?>img/icons/my_na_[57x57].png">
-	<link href='//fonts.googleapis.com/css?family=Yanone+Kaffeesatz' rel='stylesheet' type='text/css'>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-	<script src="<?php echo base_url('/'); ?>js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="<?php echo base_url('/'); ?>css/flags/flags.css">
-	<style type="text/css">
-		body {
-			padding-top: 0px;
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta content="minimum-scale=1.0, width=device-width, maximum-scale=1, user-scalable=no" name="viewport" />
+<meta name="apple-mobile-web-app-capable" content="yes">
 
-		}
+<meta name="google-site-verification" content="MMMMMMMMMMMMMMMMMM">
 
-		.form-signin {
-			max-width: 300px;
-			padding: 19px 29px 29px;
-			margin: 0 auto 20px;
-			background-color: #fff;
-			border: 1px solid #e5e5e5;
-			-webkit-border-radius: 5px;
-			-moz-border-radius: 5px;
-			border-radius: 5px;
-			-webkit-box-shadow: 0 1px 2px rgba(0, 0, 0, .05);
-			-moz-box-shadow: 0 1px 2px rgba(0, 0, 0, .05);
-			box-shadow: 0 1px 2px rgba(0, 0, 0, .05);
-		}
+<meta name="author" content="My Namibia">
 
-		.form-signin .form-signin-heading,
-		.form-signin .checkbox {
-			margin-bottom: 10px;
-		}
+<base href="<?php echo base_url('/');?>" target="_self">
 
-		.form-signin {
-			background-color: #fff;
-			margin-bottom: 10px;
-			background: #fff;
-			-moz-box-shadow: 0 0 10px #666;
-			-webkit-box-shadow: 0 0 10px #666;
-			box-shadow: 0 0 10px #666;
-			-moz-border-radius: 5px;
-			-webkit-border-radius: 5px;
-			border-radius: 5px; /* future proofing */
-			-khtml-border-radius: 5px; /* for old Konqueror browsers */
-		}
+<!--<meta name="csrf-token" content="{{ csrf_token() }}">-->
 
+<title><?php if(isset($title)){echo $title;}else{ echo 'My Namibia &trade;';}?></title>
+<meta name="description" content="<?php if(isset($metaD)){echo $metaD;}else{ echo 'My Namibia - the Free business portal in Namibia';}?>">
 
-	</style>
+<link rel="shortcut icon" type="image/ico" href="favicon.ico">
 
-	<?php
-	if ($VERIFIED == 'Y')
-	{
+  <?php if(isset($og)){ echo $og;}?>
+<link rel="publisher" href="https://plus.google.com/103433975350364100667">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url('/');?>img/icons/my_na_[144x144].png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url('/');?>img/icons/my_na_[114x114].png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url('/');?>img/icons/my_na_[72x72].png">
+  <link rel="apple-touch-icon-precomposed" href="<?php echo base_url('/');?>img/icons/my_na_[57x57].png">
 
-		$verifiedHTML = '<button id="verify_btn" class="btn btn-success pull-right"><i class="icon-ok icon-white"></i> Verified</button>';
+  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 
-	}
-	else
-	{
+<!--<link rel="icon" type="image/gif" href="animated_favicon.gif">-->
 
-		$verifiedHTML = '<buttons id="verify_btn" onclick="do_verify()" class="btn btn-danger  pull-right"><i class="icon-refresh icon-white"></i> Send Code</button>';
+<!--[if lt IE 8]>
+<script src="js/jquery-1.11.1.min.js"></script>
+<link href="css/ie7.css" rel="stylesheet" type="text/css">
+<script src="js/ie7.js"></script>
+<![endif]-->
 
-	}
-	?>
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<!--[if lt IE 10]>
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/modernizr.js"></script>
+<![endif]-->
+
+<!--[if (gte IE 9) | (!IE)]><!-->
+<script src="js/jquery-2.1.1.min.js"></script>
+<!--<![endif]-->
+<!-- css -->
+
+<link href="bootstrap/css/bootstrap.css?v=2" rel="stylesheet">
+
+<link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css">
+<link href="css/owl.theme.default.min.css" rel="stylesheet" type="text/css">
+<link href="css/style.combo.css" rel="stylesheet" type="text/css">
+<link href="css/icons.css" rel="stylesheet" type="text/css">
+<link href="<?php echo base_url('/'); ?>css/style.css?v=3" rel="stylesheet" type="text/css">
+
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="bootstrap/js/popper.min.js"></script>
+
+<script src="js/jquery.lazyload.min.js"></script>
+
+  <style type="text/css">
+    
+    body {
+      padding-top: 0px;
+    }
+
+    .form-signin {
+      max-width: 300px;
+      padding: 19px 29px 29px;
+      margin: 0 auto 20px;
+      background-color: #fff;
+      border: 1px solid #e5e5e5;
+      -webkit-border-radius: 5px;
+         -moz-border-radius: 5px;
+              border-radius: 5px;
+      -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+         -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+              box-shadow: 0 1px 2px rgba(0,0,0,.05);
+    }
+
+    .form-signin .form-signin-heading,
+    .form-signin .checkbox {
+      margin-bottom: 10px;
+    }
+
+    .form-signin input[type="text"],
+    .form-signin input[type="password"] {
+      font-size: 16px;
+      height: auto;
+      margin-bottom: 15px;
+      padding: 7px 9px;
+    }
+
+   .form-signin{background-color:#fff;
+    margin-bottom:10px;
+      background:#fff;
+    -moz-box-shadow:      0 0 10px #666;
+     -webkit-box-shadow:  0 0 10px #666;
+     box-shadow:         0 0 10px #666;
+     -moz-border-radius: 5px;
+    -webkit-border-radius: 5px;
+    border-radius: 5px; /* future proofing */
+    -khtml-border-radius: 5px; /* for old Konqueror browsers */
+
+}}
+
+</style>
+
 </head>
-
 <body>
 
 
@@ -148,7 +179,7 @@
 
 			<div class="control-group">
 				<?php echo $this->my_na_model->get_countries($DIAL_CODE, false, false, $class = '', $id = '');?>
-				<input type="text" id="cell" name="number" style="margin-top:10px; width:95%" class="span2" placeholder="eg: 0811234567" value="<?php if (isset($CLIENT_CELLPHONE))
+				<input type="text" id="cell" name="number" style="margin-top:10px; width:95%" class="form-control" placeholder="eg: 0811234567" value="<?php if (isset($CLIENT_CELLPHONE))
 				{
 					echo $CLIENT_CELLPHONE;
 				} ?>">
@@ -182,7 +213,7 @@
 					Please provide us with your email address to generate a new password
 				</div>
 
-				<input type="text" class="input-block-level" placeholder="Email" name="passemail" value="<?php if (isset($email))
+				<input type="text" class="iform-control" placeholder="Email" name="passemail" value="<?php if (isset($email))
 				{
 					echo $email;
 				} ?>"/>
@@ -205,7 +236,7 @@
 						Please set your new password and confirm it below.
 					</div>
 
-					<input type="password" class="input-block-level" name="pass1" value=""/>
+					<input type="password" class="form-control" name="pass1" value=""/>
 					<input type="hidden" name="token" value="<?php echo $token; ?>"/>
 					<input type="hidden" name="client_id" value="<?php echo $client_id; ?>"/>
 					<button class="btn btn-inverse" type="submit"><b>Set new password</b></button>
