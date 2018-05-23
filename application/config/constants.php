025aa66d9c5f498802a40ac145c1a3d4f1f65246
benwork <benpiet@intouch.com.na>
2018-05-23 14:05:58 +0200

    no message

--- application/config/constants.php
+++ application/config/constants.php
@@ -64,7 +64,7 @@ if($_SERVER['HTTP_HOST'] == 'localhost'){
 	define('CMS_URL',  'http://cms.my.na/');
 	define('CDN_URL',  'https://cdn.my.na/');
 	define('S3_URL',  'https://d3rp5jatom3eyn.cloudfront.net/');
-	define('NA_URL',  'https://www.my.na/');
+	define('NA_URL',  'https://beta.my.na/');
 	define('HUB_URL',  'https://nmh.my.na/');
 	
 }else{
@@ -73,7 +73,7 @@ if($_SERVER['HTTP_HOST'] == 'localhost'){
 	define('CMS_URL',  'https://cms.my.na/');
 	define('CDN_URL',  'https://cdn.my.na/');
 	define('S3_URL',  'https://d3rp5jatom3eyn.cloudfront.net/');
-	define('NA_URL',  'https://www.my.na/');
+	define('NA_URL',  'https://beta.my.na/');
 	define('HUB_URL',  'https://nmh.my.na/');
 }
 
