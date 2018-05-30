c976e4547ecbf4e7a13d44df2d2be4155042046b
benwork <benpiet@intouch.com.na>
2018-05-30 11:56:31 +0200

    no message

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -56,11 +56,8 @@
 
 <div class="row footer-modal animate bounce">
     <div class="container">
-    	<button type="button" class="close pull-right text-light" data-dismiss="" aria-label="Close">
+    	<button type="button" class="close pull-right text-light" data-dismiss="" aria-label="Close"><span aria-hidden="true">&times;</span></button> 
         <div class="row text center text-light" style="padding:20px;">
-          
-          <span aria-hidden="true">&times;</span>
-        </button>      	
         	<strong>Notice</strong>
 			This website or its third-party tools use cookies, which are necessary to its functioning and required to achieve the purposes illustrated in the cookie policy. If you want to know more or withdraw your consent to all or some of the cookies, please refer to the <a href="https://www.my.na/page/privacy-policy-2018/" target="_blank" style="color:#ff9900">cookie policy</a>.
 			By closing this banner, accepting, clicking a link or continuing to browse otherwise, you agree to the use of cookies. <a href="javascript:void(0)"" id="gdpr_accept" style="color:#ff9900">Accept</a> | <a href="javascript:void(0)" style="color:#ff9900">Do not Accept</a>
