546b375c1aa0b4c1f016244919de184e820237b9
benwork <benpiet@intouch.com.na>
2018-05-30 11:25:59 +0200

    no message

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -60,7 +60,7 @@
         </button>      	
         	<strong>Notice</strong>
 			This website or its third-party tools use cookies, which are necessary to its functioning and required to achieve the purposes illustrated in the cookie policy. If you want to know more or withdraw your consent to all or some of the cookies, please refer to the <a href="https://www.my.na/page/privacy-policy-2018/" target="_blank" style="color:#ff9900">cookie policy</a>.
-			By closing this banner, accepting, clicking a link or continuing to browse otherwise, you agree to the use of cookies. <a href="javascript:void(0)" style="color:#ff9900">Accept</a> | <a href="javascript:void(0)" style="color:#ff9900">Do not Accept</a>
+			By closing this banner, accepting, clicking a link or continuing to browse otherwise, you agree to the use of cookies. <a href="javascript:void(0)"" id="gdpr_accept" style="color:#ff9900">Accept</a> | <a href="javascript:void(0)" style="color:#ff9900">Do not Accept</a>
         </div>    
     </div>
 </div> 
@@ -79,6 +79,13 @@
     }
     
 ?>
+
+	$(document).on('click', '#gdpr_accept', function(e) {
+
+		alert('yay');
+
+	});
+
 
 
 $(document).ready( function(){
