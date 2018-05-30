13798ec51ff835535c1ac0b32d20b2c3bafdd2a6
benwork <benpiet@intouch.com.na>
2018-05-30 10:59:54 +0200

    no message

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -55,9 +55,12 @@
 <div class="row-fluid footer-modal animate bounce">
     <div class="container">
         <div class="row text center text-light" style="padding:20px;">
+          <button type="button" class="close" data-dismiss="" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>      	
         	<strong>Notice</strong>
 			This website or its third-party tools use cookies, which are necessary to its functioning and required to achieve the purposes illustrated in the cookie policy. If you want to know more or withdraw your consent to all or some of the cookies, please refer to the cookie policy.
-			By closing this banner, scrolling this page, clicking a link or continuing to browse otherwise, you agree to the use of cookies.
+			By closing this banner, accepting, clicking a link or continuing to browse otherwise, you agree to the use of cookies. <a href="javascript:void(0)">Accept</a>
         </div>    
     </div>
 </div> 
