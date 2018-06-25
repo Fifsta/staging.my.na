8f9f82b6cc164a5cdb493699a786a6179b05436f
Fifsta <christian@intouch.com.na>
2018-06-25 16:03:53 +0200

    no message

--- application/views/inc/adverts.php
+++ application/views/inc/adverts.php
@@ -1,5 +1,10 @@
 <div class="adverts hidden-sm-down" id="advert-box">
 	
+<div class="row" style="margin-bottom:40px">
+    <div class="col-md-12">
+		<a href="https://survey.my.na/999/survey/10" target="_blank"><img class="lazy" style="width:100%" alt="Customer Feedback Survey" src="<?php echo base_url('/'); ?>images/adverts/survey_advert.jpg" /></a>
+    </div>
+</div>	
 
 </div>
 
@@ -23,7 +28,7 @@ function load_advert(){
 		url: '<?php echo site_url('/').'my_na/load_advert/'; ?>' ,
 		success: function (data) {
 			
-			 $('#advert-box').html(data);
+			 $('#advert-box').append(data);
 			
 		}
 	});	
