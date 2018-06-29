d585d72ee3089d2133c26f6d713e06eadee1d339
Fifsta <christian@intouch.com.na>
2018-06-29 09:42:38 +0200

    no message

--- application/views/inc/news.php
+++ application/views/inc/news.php
@@ -3,8 +3,8 @@
 	<div class="heading">
 		<h2 data-icon="fa-newspaper-o">Todays <strong>Headlines</strong></h2>
 		<ul class="options">
-			<li><a href="#" data-icon="fa fa-edit text-dark">List my own</a></li>
-			<li><a href="#" data-icon="fa fa-bullhorn text-dark">Alert me</a></li>
+			<!--<li><a href="#" data-icon="fa fa-edit text-dark">List my own</a></li>
+			<li><a href="#" data-icon="fa fa-bullhorn text-dark">Alert me</a></li>-->
 		</ul>
 	</div>
     
