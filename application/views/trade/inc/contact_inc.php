77a35f5f6b10ec83384beea51e746b92b8d1ffb6
Fifsta <christian@intouch.com.na>
2018-07-03 15:45:54 +0200

    no message

--- application/views/trade/inc/contact_inc.php
+++ application/views/trade/inc/contact_inc.php
@@ -1,7 +1,7 @@
 <h2 class="tab-head">Contact Agent/Seller</h2>
 <form action="<?php echo site_url('/')?>trade/contact/" method="post" accept-charset="utf-8" id="contact-us" name="contact-us">
   <div class="row">
-    <div class="col-sm-4 col-md-3">
+    <div class="col-sm-4 col-md-3 col-lg-4">
       <div class="form-group">
         <label for="FullName">Full Name</label>
         <input id="FullName" name="name" id="name" class="form-control input-sm" placeholder="Full Name">
@@ -11,13 +11,13 @@
         <input id="EmailAddress" name="email" id="email" class="form-control input-sm" placeholder="Email Address">
       </div>
     </div>
-    <div class="col-sm-4 col-md-3">
+    <div class="col-sm-4 col-md-3 col-lg-4">
       <div class="form-group">
         <label for="Enquiry">Message / Enquiry</label>
         <textarea id="msg" name="msg" class="form-control input-sm" rows="5"></textarea>
       </div>
     </div>
-    <div class="col-sm-4 col-md-6">
+    <div class="col-sm-4 col-md-6 col-lg-4">
       
        <div class="form-group">
           <label for="EmailAddress">Security</label>
