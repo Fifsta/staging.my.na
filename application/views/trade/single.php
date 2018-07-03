92e7ed83f242c977313c5715df3fd88a3e55df22
Fifsta <christian@intouch.com.na>
2018-07-03 11:47:26 +0200

    no message

--- application/views/trade/single.php
+++ application/views/trade/single.php
@@ -156,7 +156,7 @@
 
   <div class="row">
 
-    <div class="col-sm-4 col-md-4 col-lg-3 col-xl-4 order-md-2 order-sm-1 order-lg-2 order-xl-3" id="sidebar">
+    <div class="col-sm-4 col-md-4 col-lg-3 col-xl-3 order-md-2 order-sm-1 order-lg-3 order-xl-3" id="sidebar">
       
       <?php $this->load->view('inc/login'); ?>
       <?php $this->load->view('inc/weather'); ?>
@@ -164,7 +164,7 @@
 
     </div>
 
-    <div class="col-sm-8 col-md-8 col-lg-9 col-xl-8 order-md-1 order-sm-2">
+    <div class="col-sm-8 col-md-8 col-lg-9 col-xl-9 order-md-1 order-sm-2">
 
        <section id="listing">
 
@@ -221,7 +221,7 @@
           </section>
         </div>
         <!--tabs-->   
-
+ 
         <div class="spacer"></div>
 
         <!--tabs-->
