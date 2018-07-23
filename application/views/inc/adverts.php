ad5a313b73bfccd5a869155bf69622d3a6a89c9d
Fifsta <christian@intouch.com.na>
2018-07-23 17:29:55 +0200

    no message

--- application/views/inc/adverts.php
+++ application/views/inc/adverts.php
@@ -12,12 +12,12 @@ $(document).ready(
 	function()
 	{
 
-		load_yzx('all', 4, 'side_block_1');
+		load_advert();
 
 	}
 );
 
-/*function load_advert(){
+function load_advert(){
 		
 	$.ajax({
 		type: 'post',
@@ -29,38 +29,6 @@ $(document).ready(
 		}
 	});	
 
-}*/
-
-	function load_yzx(q, l, b){
-
-		$.getJSON( "<?php echo HUB_URL;?>main/get_adverts/"+q+"/"+l+"/?bus_id=0<?php //echo BUS_ID;?>&keywords="+encodeURI(keywords)+"&category="+encodeURI(category), function( data ) {
-
-			var adb = $('#'+b), xx = 0;
-			for(var i = 0; i < data.length; i++) {
-				var obj = data[i];
-				adb.append(obj.body);
-				adverts.push(obj);
-				agent = obj.user_agent;
-			}
-
-			//MOBILE FIX
-			if(agent == 'mobile'){
-
-				for(var ii = 0; ii < data.length; ii++) {
-					var obj = data[ii];
-
-					$('#advert-box').append(obj.body);
-
-					//$('#adholder_'+ii).html(obj.body);
-
-				}
-
-			}
-			//load_content_ads();
-		});
-
-
-	}
-
+}
 
 </script>
