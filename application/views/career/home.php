819e1b45e90db9d3d7176f5c62a46963de2a54b7
Fifsta <christian@intouch.com.na>
2018-07-23 11:12:45 +0200

    no message

--- application/views/career/home.php
+++ application/views/career/home.php
@@ -1,71 +1,95 @@
-<?php $this->load->view('inc/header'); ?>
+<?php 
+
+$header['title'] = 'Careers - My Namibia';
+$header['metaD'] =  'Careers on My Namibia';
  
+//BUILD OPEN GRAPH
+$header['og'] ='
+<meta property="fb:app_id" content="287335411399195"> 
+<meta property="og:type"        content="MyNamibia:business"> 
+<meta property="og:url"         content="'.site_url('/').'vacancy/"> 
+<meta property="og:title"       content="'.$header['title'].'"> 
+<meta property="og:description" content="'.$header['metaD'].'">'; 
+
+$this->load->view('inc/header');
+
+?>
+
+<link rel="stylesheet" href="<?php echo base_url('/');?>redactor/redactor/redactor.css" />
+<link href='<?php echo base_url('/');?>css/jquery.rating.css' type="text/css" rel="stylesheet"/> 
+
 </head>
 
-<body id="top">
+<body id="top"> 
 
-<?php $this->load->view('inc/top_bar'); ?>
+<?php $this->load->view('inc/top_bar');?>
 
 <nav id="bread">
-    <div class="container">
-          <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#">My.na</a></li>
-          </ol>
-    </div>
+  <div class="container">
+    <ol class="breadcrumb">
+
+    </ol>
+  </div>
 </nav>
- 
-<div class="container"> 
-    <div class="row">
-      
-        <div class="col-sm-4 col-md-4 col-lg-3 col-xl-3 order-md-2 order-sm-1 order-lg-3 order-xl-3" id="sidebar">
-          <?php $this->load->view('inc/login'); ?>
-          <?php $this->load->view('inc/weather');?>
-          <?php $this->load->view('inc/adverts');?>
-        </div>
 
-        <div class="col-sm-8 col-md-8 col-lg-9 col-xl-9 order-md-1 order-sm-2">
-            <div class="row">
-                <div class="col-md-12">
+<div class="container">
 
+  <div class="row">
 
-                  <?php $this->load->view('career/inc/profile');?>  
+      <div class="col-sm-4 col-md-4 col-lg-3 col-xl-3 order-md-2 order-sm-1 order-lg-3 order-xl-3" id="sidebar">
+
+        <?php $this->load->view('inc/login'); ?>
+        <?php $this->load->view('inc/weather');?>
+        <?php $this->load->view('inc/adverts');?>
+
+      </div>
+
+      <div class="col-sm-8 col-md-8 col-lg-9 col-xl-9 order-md-1 order-sm-2">
 
 
-                </div>
-            </div>
-        </div>
-    </div>  
+          <?php $this->load->view('career/inc/profile');?>
+
+
+      </div>
+
+  </div>  
+  
 </div>
-    
-<?php $this->load->view('inc/footer');?>    
+  
+<div class="spacer"></div>  
+  
+<?php $this->load->view('inc/footer');?>  
 
-<script src="<?php echo base_url('/');?>js/custom/fb.js?v=1"></script>
+<script src="<?php echo base_url('/')?>redactor/redactor/redactor.min.js?v=1"></script> 
+<script src="<?php echo base_url('/');?>js/jquery.rating.pack.js" type="text/javascript"></script> 
 
 
 <script type="application/ld+json">
 {
   "@context" : "http://schema.org",
-  "@type" : "Organization",
-  "name" : "My Namibia",
-  "description" : "The biggest online portal for Namibians to find the Careers they are lookin for.",
+  "@type" : "Organization", 
+  "name" : "<?php echo $name; ?>",
+  "description" : "<?php echo strip_tags($description); ?>",
   "brand" : { 
-        "@type": "brand",
-        "image" : "https://my.na/images/logo-main.png"
+      "@type": "brand",
+    "image" : "<?php echo $img_url; ?>"
   },
-  "sameAs": [
-    "https://www.facebook.com/mynamibia/",
-    "https://twitter.com/MyNamibia"
-  ],  
-  "address": {
-        "@type": "PostalAddress",
-        "streetAddress": "11B Genl. Murtala Muhammed Ave Windhoek, Namibia"
-  },  
   "contactPoint": {
-        "@type": "ContactPoint",
-        "contactType" : "customer service",
-        "email" : "info@my.na",
-        "url" : "https://www.my.na",
-        "telephone" : "+264 61 309 591"
+      "@type": "ContactPoint",
+      "contactType" : "Customer service" ,
+      "telephone" : "<?php echo $tel; ?>" ,
+      "faxNumber" : "<?php echo $fax; ?>" ,
+      "email" : "<?php echo $email; ?>" ,
+      "url" : "<?php echo $website; ?>"
+  },
+  "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "<?php echo $address; ?>"
+  } ,
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "<?php echo $rating; ?>",
+    "ratingCount": "<?php echo $rating_count; ?>"
   }
 }
 
@@ -76,11 +100,12 @@
 {
   "@context" : "http://schema.org",
   "@type" : "GeoCoordinates", 
-  "latitude" : "-22.5480965",
-  "longitude" : "17.0872937"                      
+  "latitude" : "<?php echo $latitude; ?>",
+  "longitude" : "<?php echo $longitude; ?>"                      
 } 
 
 </script>
+
 
 </body>
 </html>
--- application/views/career/inc/profile.php
+++ application/views/career/inc/profile.php
@@ -5,37 +5,37 @@
 ?>
 <section id="listing">
 	
-	<div class="row">
-        <div class="heading" style="margin-bottom:15px">
-          <h2 data-icon="fa-briefcase">My Career profile</h2>
-          <ul class="options">    
-            <li><a href="#Enquiry-Form" data-icon="fa-envelope text-dark">Contact Agency</a></li>
-            <li><a href="#Gallery" data-icon="fa-file-image-o text-dark" onClick="load_gallery();">Gallery</a></li>
-            <li><a href="#Reviews" data-icon="fa-star text-dark">Reviews</a></li>
-            <li><a href="#QR" data-icon="fa-qrcode text-dark">QR Code</a></li>
-          </ul>
-        </div>
+    <div class="heading" style="margin-bottom:15px;">
+      <h2 data-icon="fa-briefcase">My Career profile</h2>
+      <ul class="options">    
+        <li><a href="#Enquiry-Form" data-icon="fa-envelope text-dark">Contact Agency</a></li>
+        <li><a href="#Gallery" data-icon="fa-file-image-o text-dark" onClick="load_gallery();">Gallery</a></li>
+        <li><a href="#Reviews" data-icon="fa-star text-dark">Reviews</a></li>
+        <li><a href="#QR" data-icon="fa-qrcode text-dark">QR Code</a></li>
+      </ul>
+    </div>
             
-	</div>
 
 	<div class="row">
-		<section class="results-item">
-			<div>
-				<figure>
-					<img src="<?php echo $this->my_na_model->get_user_avatar_str('200','200'); ?>" class="img-responsive">
-				</figure>
-			</div>
-			<div>
-				<div class="row">
-					<div class="col-sm-12 col-md-6 col-lg-4">
-						<div class="form-group">
-							<label>First Name</label>
-							<input type="text" class="form-control input-sm" placeholder="First Name">
+		<div class="col-md-12">
+			<section class="results-item">
+				<div>
+					<figure>
+						<img src="<?php echo $this->my_na_model->get_user_avatar_str('200','200'); ?>" class="img-responsive">
+					</figure>
+				</div>
+				<div>
+					<div class="row">
+						<div class="col-sm-12 col-md-6 col-lg-4">
+							<div class="form-group">
+								<label>Job Title</label>
+								<input type="text" class="form-control input-sm" placeholder="Job Title" value="<?php echo $bio['job_title']; ?>">
+							</div>
 						</div>
-					</div>
-				</div>	
-			</div>
-		</section>
+					</div>	
+				</div>
+			</section>
+		</div>
 	</div>
 	
 </section>
