3f2b45e3736a5ddad2e70a02adf930b66e2635e4
LAPTOP-327J8PB4\User <christian@intouch.com.na>
2018-07-25 21:53:33 +0200

    no message

--- application/controllers/Nmh.php
+++ application/controllers/Nmh.php
@@ -90,9 +90,6 @@ class Nmh extends CI_Controller {
 
 	function check_me()
 	{
-
-		$this->load->library('encryption');
-		
 		$this->output->set_header("Access-Control-Allow-Origin: *");
 		$this->output->set_header( "Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS" );
 		$this->output->set_header( 'Access-Control-Allow-Headers: content-type' );
@@ -118,8 +115,6 @@ class Nmh extends CI_Controller {
 						
 						
 					}
-
-
 					
 					$d['my_na_id'] = $id;
 					$d['u_name'] = $this->session->userdata('u_name');
@@ -130,8 +125,8 @@ class Nmh extends CI_Controller {
 					$d['points'] = $this->session->userdata('points');
 					$d['subscriptions'] = $this->session->userdata('subscriptions');
 					$d['register_date'] = $this->session->userdata('register_date');
-					echo "'".$url.'?redirect='.$redirect.'&sess='.$this->encryption->encrypt(json_encode($d))."<br />Redirect: ".$redirect."
-					<script>window.top.location.href = '".$url.'?redirect='.$redirect.'&sess='.$this->encryption->encrypt(json_encode($d))."';</script>";
+					echo "'".$url.'?redirect='.$redirect.'&sess='.$this->encrypt->encode(json_encode($d))."<br />Redirect: ".$redirect."
+					<script>window.top.location.href = '".$url.'?redirect='.$redirect.'&sess='.$this->encrypt->encode(json_encode($d))."';</script>";
 				}
 
 				//echo json_encode($this->session->all_userdata());
@@ -141,6 +136,7 @@ class Nmh extends CI_Controller {
 				//$d['msg'] = 'No session present';
 				//echo json_encode($d);
 			}
+
 
 		}else{
 
