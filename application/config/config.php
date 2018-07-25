d14766b61800a5cd8d1f88061c3df5d852686b38
LAPTOP-327J8PB4\User <christian@intouch.com.na>
2018-07-25 20:52:33 +0200

    no message

--- application/config/config.php
+++ application/config/config.php
@@ -100,7 +100,7 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'AUTO';
+$config['uri_protocol']	= 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
@@ -416,15 +416,13 @@ $config['encryption_key'] = 'myna535354refdt53';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_cookie_name']   = 'my_na';
-$config['sess_expiration']    = 0;
-$config['sess_expire_on_close'] = FALSE;
-$config['sess_encrypt_cookie']  = TRUE;
-$config['sess_use_database']  = FALSE;
-$config['sess_table_name']    = 'ci_sessions';
-$config['sess_match_ip']    = FALSE;
-$config['sess_match_useragent'] = TRUE;
-$config['sess_time_to_update']  = 30;
+$config['sess_driver'] = 'my_na';
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 0;
+$config['sess_save_path'] = NULL;
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
