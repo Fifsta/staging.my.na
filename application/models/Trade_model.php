26eb664d50e1e7505d3ce4f6f18a8005b7d14aee
Fifsta <christian@intouch.com.na>
2018-07-30 10:31:28 +0200

    no message

--- application/models/Trade_model.php
+++ application/models/Trade_model.php
@@ -9080,18 +9080,16 @@ class Trade_model extends CI_Model
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++		 	
 
 	//setlocale(LC_ALL, 'en_US.UTF8');
-	function clean_url_str($str, $replace = array(), $delimiter = '-')
-	{
-		if (! empty($replace))
-		{
-			$str = str_replace((array) $replace, ' ', $str);
+	function clean_url_str($str, $replace=array(), $delimiter='-') {
+		if( !empty($replace) ) {
+			$str = str_replace((array)$replace, ' ', $str);
 		}
-
+	
 		$clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
 		$clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
 		$clean = strtolower(trim($clean, '-'));
 		$clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
-
+	
 		return $clean;
 	}
 
