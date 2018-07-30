da7ab887c0a02f2a42f8aa52f54301cb34909018
Fifsta <christian@intouch.com.na>
2018-07-30 08:32:07 +0200

    no message

--- application/views/business/profile.php
+++ application/views/business/profile.php
@@ -181,11 +181,11 @@ $this->load->view('inc/header');
                         
 						<div class="row reveal">
 							<div class="col-sm-12 col-md-6 col-lg-4">
-								<p data-icon="fa-phone text-dark"><button onClick="phone_click($(this),'phone')" class="btn btn-default"><!--T: --><span><?php echo $tel; ?></span></button></p>
-								<p data-icon="fa-fax text-dark"><button onClick="phone_click($(this),'fax')" class="btn btn-default"><!--F: --><span><?php echo $fax; ?></span></button></p>								
+								<p data-icon="fa-phone text-dark"><button onClick="phone_click('phone')" class="btn btn-default"><!--T: --><span><?php echo $tel; ?></span></button></p>
+								<p data-icon="fa-fax text-dark"><button onClick="phone_click('fax')" class="btn btn-default"><!--F: --><span><?php echo $fax; ?></span></button></p>								
 							</div>
 							<div class="col-sm-12 col-md-6 col-lg-4">
-								<p data-icon="fa-tablet text-dark"><button onClick="phone_click($(this),'cell')" class="btn btn-default"><!--C: --><span><?php echo $cell; ?></span></button></p>
+								<p data-icon="fa-tablet text-dark"><button onClick="phone_click('cell')" class="btn btn-default"><!--C: --><span><?php echo $cell; ?></span></button></p>
 								<p data-icon="fa-envelope text-dark"><button class="btn btn-default"><!--E: --><span><?php echo $email; ?></span></button></p>								
 							</div>
 							<div class="col-sm-12 col-md-6 col-lg-4">
@@ -471,7 +471,7 @@ $this->load->view('inc/header');
 	}
 
 
-	function phone_click(n,type){
+	function phone_click(type){
 			 
 			$.ajax({
 				type: 'get',
