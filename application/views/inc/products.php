6043efcba3f4807e2c264410781dab4d8c387169
Fifsta <christian@intouch.com.na>
2018-08-06 12:23:34 +0200

    no message

--- application/views/inc/products.php
+++ application/views/inc/products.php
@@ -4,12 +4,14 @@ if($type == '3408') {
 	$title = 'Properties';
 	$section = 'properties';
 	$link = 'buy/property';
+	$sell = 'property';
 }
 
 if($type == '348') {
 	$title = 'Cars, Bikes & Boats';
 	$section = 'vehicles';
 	$link = 'buy/car-bikes-and-boats';
+	$sell = 'motor';
 }
 
 ?>
@@ -18,10 +20,9 @@ if($type == '348') {
 	<div class="heading">
 		<h2 data-icon="fa-bed"><a href="#"><strong><?php echo $title; ?></strong></a></h2>
 		<ul class="options">
-			<!--<li><a href="#" data-icon="fa fa-edit text-dark">List my own</a></li>
-			<li><a href="#" data-icon="fa fa-bullhorn text-dark">Alert me</a></li>-->
+			<li><a href="<?php echo site_url('/'); ?>sell/index/o/<?php echo $sell; ?>" data-icon="fa fa-edit text-dark">List my own</a></li>
 			<li><a href="<?php echo site_url('/').$link; ?>" data-icon="fa fa-chevron-right text-dark">All <?php echo $section; ?></a></li>
-		</ul>
+		</ul> 
 	</div>
 	
 	<div>
