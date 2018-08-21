383c8aabfbd604a85d55f2355373cd4b4afeba5f
Fifsta <christian@intouch.com.na>
2018-08-21 14:44:42 +0200

    no message

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -1,5 +1,5 @@
 
-<section id="mobile_ads" class="d-none d-sm">
+<section id="mobile_ads" class="adverts d-md-none">
 
 </section>
 
@@ -15,7 +15,6 @@
         position: fixed;
         display:none;
     }
-
 
 </style>
 
