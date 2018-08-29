61e6673ae9a468545b3345444bbaced842326cd1
Fifsta <christian@intouch.com.na>
2018-08-29 13:20:16 +0200

    no message

--- application/models/Email_model.php
+++ application/models/Email_model.php
@@ -32,6 +32,48 @@ class Email_model extends CI_Model{
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
+	function send_web_mail_get($var)
+	{
+
+
+		$config = Array(
+		    'protocol' => 'smtp',
+		    'smtp_host' => 'tls://email-smtp.eu-west-1.amazonaws.com',
+		    'smtp_port' => 465,
+		    'smtp_user' => 'AKIAIEDWIYXIABCFGGFQ',
+		    'smtp_pass' => 'Ahxb1+zvPa8Eq6zgDuZEkdhNwPBZSRQPOBSVQ/AqW7YA',
+		    'mailtype'  => 'html', 
+		    'charset'   => 'iso-8859-1'
+		);
+		$this->load->library('email', $config);
+		$this->email->set_newline("\r\n");
+
+		// Set to, from, message, etc.
+
+		$this->email->from('no-reply@intouchsrv.com');
+		$this->email->to('christian@intouch.com.na');
+		$this->email->subject('test');
+		$this->email->message('test');
+
+		$this->email->send();		
+
+		//$result = $this->email->send();
+
+
+
+		echo $this->email->print_debugger();
+
+			/*$email = array(
+				'html' => $var['body'], //Consider using a view file
+				'subject' => $var['subject'],
+				'headers' => array('Reply-To' => $var['from_email']),
+				'from_email' => 'vw-no-reply@my.na',
+				'from_name' => $var['name'],
+				'to' => $sendTo
+			);*/	
+
+	}
+
 	//NEW SERVER
 	function send_enquiry2()
 	{
