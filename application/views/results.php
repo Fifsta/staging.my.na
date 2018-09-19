213ae56e2b44cbeb1115f210b858d220f4f8215f
Fifsta <christian@intouch.com.na>
2018-09-19 11:44:56 +0200

    no message

--- application/views/results.php
+++ application/views/results.php
@@ -164,7 +164,7 @@ $this->load->view('inc/header', $header);
 
 <script src='<?php echo base_url('/')?>js/jquery.rating.pack.js' type="text/javascript" language="javascript"></script>
 
-<script src="https://maps.googleapis.com/maps/api/js?sensor=true&key=AIzaSyBNrRasxiR5Oo4UZ2XsEGZghmAt_qX2nl8" async defer></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBNrRasxiR5Oo4UZ2XsEGZghmAt_qX2nl8" async defer></script>
 
 <script src="<?php echo base_url('/');?>js/geolocationmarker-compiled.js"></script>
 
